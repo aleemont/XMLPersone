0009735cf064a7f3fa9834d41abcbc01585dc3be
Alessandro Monticelli <alexmonticelli02@gmail.com>
2020-05-28 14:46:40 +0000

    Fixed loading files issues

--- persone.js
+++ persone.js
@@ -1,14 +1,13 @@
 var ppl = new Array();
-
 function load(){
-    var xmlhttp = new XMLHttpRequest;
+    var xmlhttp = new XMLHttpRequest();
     var url = "data.txt";
 
     xmlhttp.onreadystatechange = function(){
         if(xmlhttp.readyState == 4){
             if(xmlhttp.status == 200){
-                persone = JSON.parse(xmlhttp.responseText);
-                load();
+                ppl = JSON.parse(xmlhttp.responseText);
+                print();
             }
             else
                 document.getElementById("error").innerHTML = "ERROR";
@@ -17,10 +16,8 @@ function load(){
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
-
 function print(){
-    /*here is where i should create the table
-     where to put the content of data.txt*/
+
 }
 
 
